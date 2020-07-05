c2662e92079519ec8e7633b8bcd9e01b4ff51cd3
Anxhelo <angelo.diko12@gmail.com>
2020-07-05 15:52:03 +0200

    New dataset

--- SOURCE/dataset.py
+++ SOURCE/dataset.py
@@ -53,7 +53,7 @@ class CT_Triplet(Dataset):
                         
     """
     
-    def __init__(self, root_dir, split_covid, split_nonCovid, transform=None):
+    def __init__(self, root_dir, split_covid, split_nonCovid, transform=None, train=True):
         
         self.root_dir = root_dir
         self.split_covid = split_covid
@@ -64,10 +64,20 @@ class CT_Triplet(Dataset):
         covid_list = read_file(os.path.join(self.root_dir, 'CT_COVID',self.split_covid))
         nonCovid_list  = read_file(os.path.join(self.root_dir, 'CT_NonCOVID',self.split_nonCovid))
         self.img_list = covid_list + nonCovid_list
-        
+        self.train = train
+
         CovidLabels = set_label(covid_list, 'CT_COVID')
         NonCovidLabels = set_label(nonCovid_list)
         self.labels = CovidLabels + NonCovidLabels
+
+        self.anchor_transform  = transforms.Compose([
+                                  transforms.Resize((224,224)),
+                                  transforms.RandomResizedCrop((224,224)), #224 is the size of the imagenet images so this will allow to use transfer learning
+                                  transforms.RandomHorizontalFlip(p=0.5), #Randomly horziontal flip image with probability p, default p = 0.5
+                                  transforms.ToTensor(),#Transforms the image to pytorch tensor (by default the values are in converted in range [0,1])
+                                  transforms.Normalize(mean=[0.45271412, 0.45271412, 0.45271412],
+                                     std=[0.33165374, 0.33165374, 0.33165374])
+                              ])
         
     def __len__(self):
         return len(self.img_list)
@@ -105,33 +115,46 @@ class CT_Triplet(Dataset):
         image_path = os.path.join(self.root_dir,self.labels_[int(self.labels[idx])],self.img_list[idx].rstrip())
         image_a = Image.open(image_path).convert('RGB')            
         label = self.labels[idx]
+
+        if self.train == True:
         
+          idx_p = self.getPositive(idx, label)
+          image_path_p = os.path.join(self.root_dir,self.labels_[int(self.labels[idx_p])],self.img_list[idx_p].rstrip())
+          image_p = Image.open(image_path_p).convert('RGB')
+          label_p = self.labels[idx_p]
         
-        idx_p = self.getPositive(idx, label)
-        image_path_p = os.path.join(self.root_dir,self.labels_[int(self.labels[idx_p])],self.img_list[idx_p].rstrip())
-        image_p = Image.open(image_path_p).convert('RGB')
-        label_p = self.labels[idx_p]
-        
-        idx_n = self.getNegative(idx, label)
-        image_path_n = os.path.join(self.root_dir,self.labels_[int(self.labels[idx_n])],self.img_list[idx_n].rstrip())
-        image_n = Image.open(image_path_n).convert('RGB')
-        label_n = self.labels[idx_n]
+          idx_n = self.getNegative(idx, label)
+          image_path_n = os.path.join(self.root_dir,self.labels_[int(self.labels[idx_n])],self.img_list[idx_n].rstrip())
+          image_n = Image.open(image_path_n).convert('RGB')
+          label_n = self.labels[idx_n]
         
         # print("anchor :", label)
         # print("positive",label_p)
         # print("negative", label_n)
         
                 
-        if self.transform != None:
-            # image_a = self.anchor_transform(image_a)
-            image_a = self.transform(image_a)
-            image_p = self.transform(image_p)
-            image_n = self.transform(image_n)
+          if self.transform != None:
+              # image_a = self.anchor_transform(image_a)
+              image_a = self.transform(image_a)
+              image_p = self.transform(image_p)
+              image_n = self.transform(image_n)
+        
+        else:
+          image_p = image_a  
+          idx_n = self.getNegative(idx, label)
+          image_path_n = os.path.join(self.root_dir,self.labels_[int(self.labels[idx_n])],self.img_list[idx_n].rstrip())
+          image_n = Image.open(image_path_n).convert('RGB')
+          label_n = self.labels[idx_n]
+
+          if self.transform != None:
+              image_a = self.anchor_transform(image_a)
+              # image_a = self.transform(image_a)
+              image_p = self.transform(image_p)
+              image_n = self.transform(image_n)
+        
             
         
-        return image_a, image_p, image_n , label, label_p, label_n
-
-
+        return image_a, image_p, image_n , label, label, label_n
 
 class CT_Images(Dataset): #single image batches
     
